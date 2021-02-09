46ea9603daa6dfc000201911329339b6e056767e
Stéphane Englebert <stephane.englebert@gmail.com>
2021-02-09 20:02:40 +0100

    update js

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -6,8 +6,15 @@
     }
     function initArrayWords(){
         // ENCODE MORE WORDS
-        let arrayWords = ["heart","description","blue","christmas","balloon","beautiful","flower"];
-        arrayWords = arrayWords.concat(["pudding","football","snack","king","snake"]);
+        let arrayWords = [];
+        if(localStorage.language == "fr"){
+            arrayWords = ["coeur","vignette","tasse","lampe","voiture","superbe","dentelle"];
+            arrayWords = arrayWords.concat(["chapeau","cheveux","maison","grenouille","serpent"]);
+        }
+        if(localStorage.language == "en"){
+            arrayWords = ["heart","description","blue","christmas","balloon","beautiful","flower"];
+            arrayWords = arrayWords.concat(["pudding","football","snack","king","snake"]);
+        }
         return arrayWords;
     }
     function initWordToGuess(){
@@ -39,7 +46,7 @@
     function initTries(){
         // TO-DO
         let target = document.getElementById("tries");
-        target.innerHTML = localStorage.triesNb;
+        target.innerHTML = localStorage.triesNb + "/" + localStorage.maxTries;
     }
     function initWordPanel(wordToGuess){
         // TO-DO
@@ -49,41 +56,35 @@
     function initKeyboard(){
         // TO-DO
         let target = document.getElementById("keyboardContainer");
-        let choosenLettersTrue = localStorage.choosenLettersTrue;
-        let choosenLettersFalse = localStorage.choosenLettersFalse;
+        target.innerHTML = "";
         let keyboardKeys = localStorage.keyboardKeys.split(",");
         keyboardKeys.forEach(key => {
             let keyContainer = document.createElement("div");
             keyContainer.innerHTML = key;
             keyContainer.className = "col-2 col-md-1 kbKeys ";
-            keyContainer.id = "key_" + key;
+            keyContainer.id = "key_" + key.toUpperCase();
             keyContainer.style.cursor = "pointer";
-            if(choosenLettersFalse.includes(key)){
-                keyContainer.style.color = "red";
-                keyContainer.style.border = "1px solid red";
-                keyContainer.style.borderRadius = "4px";
-                keyContainer.style.cursor = "auto";
-            }else if(choosenLettersTrue.includes(key)){
-                keyContainer.style.color = "green";
-                keyContainer.style.border = "1px solid green";
-                keyContainer.style.borderRadius = "4px";
-                keyContainer.style.cursor = "auto";
-            }else{
-                keyContainer.className += "zoomKey";
-            }
+            keyContainer.className += "zoomKey";
             target.appendChild(keyContainer);
         })
     }
     function initLocalStorage(){
         localStorage.setItem("wordToGuess","");
+        localStorage.setItem("language","fr");
+        localStorage.setItem("nbPlayedGames",0);
+        localStorage.setItem("bestScore",0);
         localStorage.setItem("gameOver",false);
         localStorage.setItem("gameResult","unknown") // values: unknown, won, lost
         localStorage.setItem("score",0);
+        localStorage.setItem("simpleBonus",100);
         localStorage.setItem("triesNb",0);
         localStorage.setItem("maxTries",6);
         localStorage.setItem("choosenLettersTrue",[]);
         localStorage.setItem("choosenLettersFalse",[]);
-        localStorage.setItem("keyboardKeys",["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]);
+        //localStorage.setItem("keyboardKeys",["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]);
+        localStorage.setItem("keyboardKeys",["A","Z","E","R","T","Y","U","I","O","P","Q","S","D","F","G","H","J","K","L","M","W","X","C","V","B","N"]);
+        localStorage.setItem("keyboard-fr",["A","Z","E","R","T","Y","U","I","O","P","Q","S","D","F","G","H","J","K","L","M","W","X","C","V","B","N"]);
+        localStorage.setItem("keyboard-en",["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]);
     }
     // The function 'wordGuessed' checks if every letters have been found by user and returns a boolean
     function wordGuessed(){
@@ -97,67 +98,132 @@
     }
     function messageGameOver(){
         // TO-DO
-
+        document.getElementById("gamePanel").style.display = "none";
+        let contentMsgPanel = "";
+        let message = "";
+        if(localStorage.gameResult == "won"){
+            switch(localStorage.language){
+                case "fr": message += "Félicitations! <br/>Vous avez trouvé le mot caché!"; break;
+                case "en": message += "Congratulations! <br/>You found the hidden word!";break;
+            }
+        }else{
+            switch(localStorage.language){
+                case "fr": message += "Partie perdue.<br/>Essayez à nouveau."; break;
+                case "en": message += "Game over.<br/>Try again.";break;
+            }
+        }
+        contentMsgPanel += message;
+        document.getElementById("messageContainer").innerHTML = contentMsgPanel;
+        document.getElementById("messagePanel").style.display = "block";
+        localStorage.nbPlayedGames++;
+    }
+    function showFoundLetters(){
+        let wordToGuess = localStorage.wordToGuess.split("");
+        let foundLetters = "";
+        wordToGuess.forEach(letter => {
+            let letterTrue = localStorage.choosenLettersTrue.includes(letter.toUpperCase());
+            if(!letterTrue){foundLetters += "_";}else{foundLetters += letter.toUpperCase();}
+        })
+        initWordPanel(foundLetters);
+    }
+    function updateScore(){
+        // TO-DO
+        let score = parseInt(localStorage.score);
+        let bonus = parseInt(localStorage.simpleBonus);
+        let finalScore = score + bonus;
+        localStorage.score = finalScore;
+        if(finalScore > localStorage.bestScore){localStorage.bestScore = finalScore;}
+        initScore();
+    }
+    function updateKeyboardKey(key){
+        let choosenLettersTrue = [];
+        if(localStorage.choosenLettersTrue.length>0){
+            choosenLettersTrue = localStorage.choosenLettersTrue.split(",");
+        }
+        let choosenLettersFalse = [];
+        if(localStorage.choosenLettersFalse.length>0){
+            choosenLettersFalse = localStorage.choosenLettersFalse.split(",");
+        }
+        let keyContainer = document.getElementById("key_" + key);
+        if(choosenLettersFalse.includes(key.toUpperCase())){
+            keyContainer.style.color = "red";
+            keyContainer.style.border = "1px solid red";
+            keyContainer.style.borderRadius = "4px";
+            keyContainer.style.cursor = "auto";
+        }else if(choosenLettersTrue.includes(key.toUpperCase())){
+            keyContainer.style.color = "green";
+            keyContainer.style.border = "1px solid green";
+            keyContainer.style.borderRadius = "4px";
+            keyContainer.style.cursor = "auto";
+        }
     }
     function checkKeyProposal(key){
         // TO-DO
+        console.clear();
         let isGameOver = (localStorage.gameOver === "true");
-        let choosenLettersTrue = localStorage.choosenLettersTrue.split(",");
-        let choosenLettersFalse = localStorage.choosenLettersFalse.split(",");
+        let choosenLettersTrue = [];
+        if(localStorage.choosenLettersTrue.length>0){
+            choosenLettersTrue = localStorage.choosenLettersTrue.split(",");
+        }
+        let choosenLettersFalse = [];
+        if(localStorage.choosenLettersFalse.length>0){
+            choosenLettersFalse = localStorage.choosenLettersFalse.split(",");
+        }
         // La partie est-elle toujours en cours?
         if(!isGameOver){
             // lettre déjà proposée?
             let alreadyProposed = false;
             if(localStorage.choosenLettersTrue.includes(key)){alreadyProposed = true;}
             if(localStorage.choosenLettersFalse.includes(key)){alreadyProposed = true;}
-            console.clear();
             if(!alreadyProposed){
-                // Si pas encore proposée, est-elle comprise dans le mot à deviner?
                 if(localStorage.wordToGuess.includes(key.toLowerCase())){
-                    console.log("Lettre '"+key+"' contenue dans le mot!");
+                    // La lettre proposée est comprise dans le mot à deviner
                     choosenLettersTrue.push(key);
                     localStorage.choosenLettersTrue = choosenLettersTrue.toString() ;
-                    // Vérifier si le mot n'a pas été trouvé (fin partie)
-                    console.log("Word guessed? > " + wordGuessed());
+                    updateScore();
+                    showFoundLetters();
                     if(wordGuessed()){
-                        // Si mot trouvé, fin de la partie
-                    }else{
-                        // Si mot pas trouvé, on continue
+                        // Le mot est trouvé, fin de la partie (victoire)
+                        localStorage.gameOver = "true";
+                        localStorage.gameResult = "won";
+                        messageGameOver();
                     }
                 }else{
-                    console.log("Lettre '"+key+"' non présente dans le mot!");
+                    // La lettre proposée n'est pas comprise dans le mot à deviner
                     choosenLettersFalse.push(key);
                     localStorage.choosenLettersFalse = choosenLettersFalse.toString();
                     localStorage.triesNb++;
-                    // Vérifier si cet essai supplémentaire ne marque pas la fin de la partie...
-                    if(localStorage.triesNb<localStorage.maxTries){
-                        console.log("Tries "+localStorage.triesNb+"/"+localStorage.maxTries+" > Game continuing...");
-                    }else{
+                    initTries()
+                    initHangingScene();
+                    if(localStorage.triesNb >= localStorage.maxTries){
+                        // Plus d'essais disponibles, fin de la partie (défaite)
                         localStorage.gameOver = "true";
                         localStorage.gameResult = "lost";
                         messageGameOver();
                     }
                 }
-                console.log("choosenLettersTrue ("+typeof localStorage.choosenLettersTrue+")");
-                console.table(localStorage.choosenLettersTrue);
-                console.log("choosenLettersFalse");
-                console.log(localStorage.choosenLettersFalse);
-                console.log("Nb of tries > " + localStorage.triesNb);
+                updateKeyboardKey(key);
             }
+            console.log(choosenLettersTrue);
+            console.log(choosenLettersFalse);
         }
+    }
+    function togglePanels(){
+        if()
     }
     window.onload = function () {
         // initialisation
-        initLocalStorage();
+        if(!localStorage.nbPlayedGames>0){initLocalStorage();}
         let wordToGuess = initWordToGuess();
         initHangingScene();
         initScore();
         initTries();
         initKeyboard();
         localStorage.keyboardKeys.split(",").forEach(key => {
-            document.getElementById("key_"+key).addEventListener("click", () => {
-                checkKeyProposal(key);
+            document.getElementById("key_"+key.toUpperCase()).addEventListener("click", () => {
+                checkKeyProposal(key.toUpperCase());
             })
         })
+        document.getElementById("play").addEventListener("click", () => togglePanels());
     }
 })();
